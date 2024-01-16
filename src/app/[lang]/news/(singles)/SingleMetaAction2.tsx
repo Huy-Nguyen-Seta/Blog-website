2cea989d77401f42842817ab5ed75b96b67416a4
Nguyen Huy Seta <nguyenquanghuy06102000@gmail.com>
2024-01-16 13:19:12 +0700

    update:share mobile

--- src/app/[lang]/news/(singles)/SingleMetaAction2.tsx
+++ src/app/[lang]/news/(singles)/SingleMetaAction2.tsx
@@ -17,27 +17,9 @@ const SingleMetaAction2: FC<SingleMetaAction2Props> = ({
   className = '',
   data,
 }) => {
-  const isMobile = () => {
-    const toMatch = [
-      /Android/i,
-      /webOS/i,
-      /iPhone/i,
-      /iPad/i,
-      /iPod/i,
-      /BlackBerry/i,
-      /Windows Phone/i,
-    ];
-
-    return toMatch.some((toMatchItem) => {
-      return navigator.userAgent.match(toMatchItem);
-    });
-  };
   const handleOnClick = (e: any) => {
     e.preventDefault();
-    console.log('clg', navigator.share);
-    if (navigator.share && true) {
-      console.log('clg2');
-
+    if (navigator.share) {
       navigator
         .share({
           title: `${data?.title}`,
@@ -54,7 +36,7 @@ const SingleMetaAction2: FC<SingleMetaAction2Props> = ({
   const lang = useTrans();
   return (
     <div className={`nc-SingleMetaAction2 ${className}`}>
-      <div className="flex flex-row space-x-2.5 rtl:space-x-reverse items-center">
+      <div className="flex flex-row md:space-x-2.5 rtl:space-x-reverse items-center">
         <PostCardLikeAndComment
           itemClass="px-4 h-9 text-sm"
           hiddenCommentOnMobile
@@ -74,7 +56,7 @@ const SingleMetaAction2: FC<SingleMetaAction2Props> = ({
           containerClassName="h-9 w-9 bg-neutral-100 hover:bg-neutral-200 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:text-neutral-200"
         />
         <NcDropDown
-          className="flex-shrink-0 flex items-center justify-center focus:outline-none h-9 w-9 bg-neutral-100 hover:bg-neutral-200 dark:bg-neutral-800 dark:hover:bg-neutral-700 text-neutral-700 dark:text-neutral-200 rounded-full"
+          className="flex-shrink-0 hidden md:flex items-center justify-center focus:outline-none h-9 w-9 bg-neutral-100 hover:bg-neutral-200 dark:bg-neutral-800 dark:hover:bg-neutral-700 text-neutral-700 dark:text-neutral-200 rounded-full"
           renderTrigger={() => (
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -91,7 +73,7 @@ const SingleMetaAction2: FC<SingleMetaAction2Props> = ({
               />
             </svg>
           )}
-          onClick={handleOnClick}
+          onClick={() => {}}
           data={SOCIALS_DATA}
           url={
             window
@@ -99,7 +81,26 @@ const SingleMetaAction2: FC<SingleMetaAction2Props> = ({
               : '#'
           }
         />
-                <button onClick={handleOnClick}>test</button>
+        <button
+          onClick={handleOnClick}
+          className="flex-shrink-0 flex items-center justify-center focus:outline-none h-9 w-9 bg-neutral-100 hover:bg-neutral-200 dark:bg-neutral-800 dark:hover:bg-neutral-700 text-neutral-700 dark:text-neutral-200 rounded-full md:hidden"
+        >
+          {' '}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={1.5}
+              d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
+            />
+          </svg>
+        </button>
 
         <PostActionDropdown
           containerClassName="h-9 w-9 bg-neutral-100 hover:bg-neutral-200 dark:bg-neutral-800 dark:hover:bg-neutral-700"
