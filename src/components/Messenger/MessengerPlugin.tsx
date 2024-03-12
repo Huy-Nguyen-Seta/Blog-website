9f84fb35767d6f0cb752685ee4985ddf7ca48cd5
seta-iris <nnguyen5@veritone.com>
2024-03-12 18:07:31 +0700

    update script

--- src/components/Messenger/MessengerPlugin.tsx
+++ src/components/Messenger/MessengerPlugin.tsx
@@ -1,4 +1,3 @@
-'use client'
 import Script from 'next/script';
 
 export default function ChatBot() {
@@ -22,6 +21,7 @@ export default function ChatBot() {
       
             (function(d, s, id) {
               var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
               js = d.createElement(s); js.id = id;
               js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
               fjs.parentNode.insertBefore(js, fjs);
