2e21765cc82fdc17058bb106180598fecd5737cb
seta-iris <nnguyen5@veritone.com>
2024-03-13 05:21:44 +0700

    update:mess

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -104,7 +104,7 @@ export default function RootLayout({
       <body className="">
         <div className="bg-[#f8f8f8] text-base dark:bg-neutral-900/95 text-neutral-900 dark:text-neutral-200">
           <SiteHeader />
-          <Facebook1 />
+          <Facebook1 lang={params?.lang} />
           <StoreProvider> {children}</StoreProvider>
           <Footer lang={params?.lang} />
           <ToastContainer
