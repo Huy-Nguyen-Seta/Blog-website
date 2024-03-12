1798eff124f6bbdeb21e0f7c215b9aa95589e54b
seta-iris <nnguyen5@veritone.com>
2024-03-13 05:32:34 +0700

    update script

--- src/components/Messenger/MessengerPlugin.jsx
+++ src/components/Messenger/MessengerPlugin.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect } from 'react';
-
+const mapLanguage = {ja: 'ja_JP	', en : 'en_US', vi : 'vi_VN'}
 /**
  *
  */
@@ -22,10 +22,11 @@ export function init(lang) {
   (function (d, s, id) {
     var js,
       fjs = d.getElementsByTagName(s)[0];
+      console.log('mapLanguage(lang)', mapLanguage[lang])
     if (d.getElementById(id)) return;
     js = d.createElement(s);
     js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    js.src = `https://connect.facebook.net/${mapLanguage[lang]}/sdk/xfbml.customerchat.js`;
     fjs.parentNode.insertBefore(js, fjs);
   })(document, 'script', 'facebook-jssdk');
 }
