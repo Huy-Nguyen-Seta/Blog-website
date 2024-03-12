8d17987b0692c3abae71a6b309bbe8b946c5fcd1
seta-iris <nnguyen5@veritone.com>
2024-03-12 17:30:04 +0700

    update script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-
+import Script from 'next/script';
 import '../app/[lang]/globals.css';
 import { Metadata } from 'next';
 import { getData } from '@/components/utils/fetch-api';
@@ -97,6 +97,28 @@ export default function RootLayout({ children }: Props) {
   return (
     <html>
       <body>{children}</body>
+      <Script strategy="lazyOnload" async id="facebook">
+        {`
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "274161719106282");
+            chatbox.setAttribute("attribution", "biz_inbox");
+      
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v12.0'
+              });
+            };
+      
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        `}
+      </Script>
     </html>
   );
 }
