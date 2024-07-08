9d6cc0819c9833f416c2cf9275471eecd867412f
seta-iris <nnguyen5@veritone.com>
2024-07-09 04:38:21 +0700

    update

--- src/components/Messenger/MessengerPlugin.jsx
+++ src/components/Messenger/MessengerPlugin.jsx
@@ -5,62 +5,61 @@ const mapLanguage = { ja: 'ja_JP	', en: 'en_US', vi: 'vi_VN' };
 /**
  *
  */
-export function init(lang) {
-  var chatbox = document.getElementById('fb-customer-chat');
-  chatbox?.setAttribute(
-    'page_id',
-    lang === 'vi' ? '460761507632325' : '657002014414624'
-  ); // TODO: move to args
-  chatbox?.setAttribute('attribution', 'biz_inbox');
+// export function init(lang) {
+//   var chatbox = document.getElementById('fb-customer-chat');
+//   chatbox?.setAttribute(
+//     'page_id',
+//     lang === 'vi' ? '460761507632325' : '657002014414624'
+//   ); // TODO: move to args
+//   chatbox?.setAttribute('attribution', 'biz_inbox');
 
-  window.fbAsyncInit = function () {
-    FB.init({
-      xfbml: true,
-      version: 'v11.0',
-    });
-  };
+//   window.fbAsyncInit = function () {
+//     FB.init({
+//       xfbml: true,
+//       version: 'v11.0',
+//     });
+//   };
 
-  (function (d, s, id) {
-    var js,
-      fjs = d.getElementsByTagName(s)[0];
-    console.log('mapLanguage(lang)', mapLanguage[lang]);
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = `https://connect.facebook.net/${mapLanguage[lang]}/sdk/xfbml.customerchat.js`;
-    fjs.parentNode.insertBefore(js, fjs);
-  })(document, 'script', 'facebook-jssdk');
-}
+//   (function (d, s, id) {
+//     var js,
+//       fjs = d.getElementsByTagName(s)[0];
+//     console.log('mapLanguage(lang)', mapLanguage[lang]);
+//     if (d.getElementById(id)) return;
+//     js = d.createElement(s);
+//     js.id = id;
+//     js.src = `https://connect.facebook.net/${mapLanguage[lang]}/sdk/xfbml.customerchat.js`;
+//     fjs.parentNode.insertBefore(js, fjs);
+//   })(document, 'script', 'facebook-jssdk');
+// }
 
-/**
- *
- */
-export function cleanup() {
-  (function (d, id) {
-    var target = d.getElementById(id);
-    if (target) {
-      target.parentNode.removeChild(target);
-    }
-  })(document, 'facebook-jssdk');
+// /**
+//  *
+//  */
+// export function cleanup() {
+//   (function (d, id) {
+//     var target = d.getElementById(id);
+//     if (target) {
+//       target.parentNode.removeChild(target);
+//     }
+//   })(document, 'facebook-jssdk');
 
-  delete window?.FB;
-}
+//   delete window?.FB;
+// }
 
 export function Facebook1({ lang }) {
-  useEffect(() => {
-    console.log('Facebook1');
-    init(lang);
+  // useEffect(() => {
+  //   console.log('Facebook1');
+  //   init(lang);
 
-    return () => {
-      cleanup();
-    };
-  }, []);
+  //   return () => {
+  //     cleanup();
+  //   };
+  // }, []);
 
   return (
     <div>
-      <div id="fb-root"></div>
-
-      <div id="fb-customer-chat" className="fb-customerchat"></div>
+      {/* <div id="fb-root"></div> */}
+      {/* <div id="fb-customer-chat" className="fb-customerchat"></div> */}
       <div className="fixed bottom-10 md:bottom-16 right-1 md:right-6 z-50">
         <div className="flex flex-col items-center justify-center space-y-4">
           <a
